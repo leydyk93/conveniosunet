a0b7db35b9ad83fb0a9bc0096b4a3bd99108639b
leydyk93 <leydykm93@gmail.com>
2016-03-23 18:01:48 -0430

    no importa 2

--- protected/data/conveniosunet.sql
+++ protected/data/conveniosunet.sql
@@ -3,6 +3,21 @@ CREATE TABLE usuario (
     nombre VARCHAR(128) NOT NULL,
     clave VARCHAR(128) NOT NULL,
     correo VARCHAR(128) NOT NULL,
+    fecha_creacion DATETIME NOT NULL,
+    IdRol VARCHAR(10) NOT NULL,
+    foreign key (`IdRol`) references `roles` (`ID`) on delete cascade on update cascade
+);
+
+INSERT INTO usuario (nombre, clave, correo, fecha_creacion,IdRol) VALUES ('test1', 'pass1', 'test1@example.com','2016/01/01','2');
+INSERT INTO usuario (nombre, clave, correo, fecha_creacion,IdRol) VALUES ('admin', '1234', 'admin@example.com','2016/01/01','1');
+INSERT INTO usuario (nombre, clave, correo, fecha_creacion,IdRol) VALUES ('leydy', '1234', 'leydy@example.com','2016/01/01','1');
+INSERT INTO usuario (nombre, clave, correo, fecha_creacion,IdRol) VALUES ('tyson', '1234', 'tyson@example.com','2016/01/01','1');
+
+CREATE TABLE usuario (
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(128) NOT NULL,
+    clave VARCHAR(128) NOT NULL,
+    correo VARCHAR(128) NOT NULL,
     fecha_creacion DATETIME NOT NULL 
 );
 
