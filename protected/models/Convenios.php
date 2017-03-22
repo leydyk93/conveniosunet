91dbdb0529a9df44d4220aba9e29a5d8f04a2a3f
tysoncardelli <tyson.cardelli@unet.edu.ve>
2017-03-22 09:40:44 -0300

    calculo de id

--- protected/models/Convenios.php
+++ protected/models/Convenios.php
@@ -152,4 +152,16 @@ class Convenios extends CActiveRecord
 	{
 		return parent::model($className);
 	}
+
+	/** */
+
+	public static function maxId() {
+
+		
+	  return Yii::app()->db->createCommand()
+	    ->select('MAX(CAST(idConvenio as int))')
+	    ->from('convenios')
+	    ->queryScalar();
+	}
+
 }
